221827c407fa9df9bcadcfb1dd8ad885c54dfe90
sudipto-basak <sudipto.im@gmail.com>
2019-01-23 16:35:03 -0500

    Call Rekognition from Lambda

--- videolyzer/videolyzer/handler.py
+++ videolyzer/videolyzer/handler.py
@@ -1,7 +1,27 @@
-import json
+import urllib
+import boto3
+
+
+def start_label_detection(bucketname, key):
+    reko = boto3.client('rekognition')
+
+    response = reko.start_label_detection(
+        Video={
+            'S3Object': {
+                'Bucket': bucketname,
+                'Name': key
+            }
+        })
+
+    print(response)
+    return
 
 
 def start_processing_video(event, context):
-    print(event)
+    for record in event['Records']:
+        start_label_detection(
+            record['s3']['bucket']['name'],
+            urllib.parse.unquote_plus(record['s3']['object']['key'])
+        )
 
     return

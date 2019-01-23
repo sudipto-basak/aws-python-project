1c9adbbf7ad713e51904ce4b077434cf4b7537ef
sudipto-basak <sudipto.im@gmail.com>
2019-01-23 12:50:07 -0500

    Change in Lambda function to include config file

--- autonotif/autoscaling.py
+++ autonotif/autoscaling.py
@@ -3,4 +3,5 @@ import boto3
 session = boto3.Session(profile_name='eclipsedev')
 asc = boto3.client('autoscaling')
 
-asc.execute_policy(AutoScalingGroupName='autonotif-asg', PolicyName='scale-up')
+response = asc.execute_policy(AutoScalingGroupName='autonotif-asg', PolicyName='scale-up')
+print(response)

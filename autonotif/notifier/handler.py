7c67955500d2f43c44684f186ab432250ec266da
sudipto-basak <sudipto.im@gmail.com>
2019-01-23 13:52:46 -0500

    Slack fix

--- autonotif/notifier/handler.py
+++ autonotif/notifier/handler.py
@@ -1,10 +1,16 @@
 import os
+import requests
+
 
 def post_to_slack(event, context):
     slack_webhook_url =  os.environ['SLACK_WEBHOOK_URL']
-    print("Slack URL :- {}".format(slack_webhook_url))
+    # print("Slack URL :- {}".format(slack_webhook_url))
+    # print(event)
 
-    print(event)
+    slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+
+    data = {"text": slack_message}
+    requests.post(slack_webhook_url, json=data)
 
     return
 

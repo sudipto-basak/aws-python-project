7063d2f57e737b8332694c0298f1e285bfa7feb5
sudipto-basak <sudipto.im@gmail.com>
2019-01-24 13:47:54 -0500

    dynamodb setup

--- videolyzer/videolyzer/handler.py
+++ videolyzer/videolyzer/handler.py
@@ -1,6 +1,7 @@
 import urllib
 import boto3
 import os
+import json
 
 
 def start_label_detection(bucketname, key):
@@ -22,6 +23,28 @@ def start_label_detection(bucketname, key):
     return
 
 
+def get_video_labels(job_id):
+    reko = boto3.client('rekognition')
+
+    result = reko.get_label_detection(JobId=job_id)
+
+    next_token = result.get('NextToken', None)
+
+    while next_token:
+
+        next_page = reko.get_label_detection(JobId=job_id, NextToken=next_token)
+
+        result['Labels'].extend(next_page['Labels'])
+        next_token = next_page.get('NextToken', None)
+
+    return result
+
+
+def put_labels_in_db(response, s3_object, s3_bucket, job_status):
+    pass
+
+
+# Lambda Events
 def start_processing_video(event, context):
     for record in event['Records']:
         start_label_detection(
@@ -32,6 +55,16 @@ def start_processing_video(event, context):
     return
 
 def handle_lable_detection(event, context):
-    print(event)
+    for record in event['Records']:
+        message = json.loads(record['Sns']['Message'])
+        job_id = message['JobId']
+        job_status = message['JobId']
+        s3_bucket = message['Video']['S3Bucket']
+        s3_object = message['Video']['S3ObjectName']
+
+        response = get_video_labels(job_id)
+        print(response)
+        put_labels_in_db(response, s3_object, s3_bucket, job_status)
+
 
     return
